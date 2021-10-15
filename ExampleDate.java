0355806faa75f2e70a33ac4d5b54aee7b2843022
AriFaiz <90914371+AriFaiz@users.noreply.github.com>
2021-10-15 13:45:44 +0700

    Add files via upload
    Mengimplementasikan Code lab DateTime dalam package dicoding.javafundamental.datetime
    Pada studi kasus ini saya belajar bagaimana menggunakan, menentukan, merubah serta menampilkan dengan format waktu belahan dunia yang diinginkan dan membangun sebuah source dalam bentuk file antara lain :
    - ExamoleCalendar
    - ExampleDate
    - ExampleSimpleDateFormat
    - ExampleSpecificCalendar
    - ExampleSplitCalendar
    - ExampleSystemCurrentMilis

---
+++ ExampleDate.java
@@ -0,0 +1,12 @@
+package com.dicoding.javafundamental.datetime;
+
+import java.util.Date;
+
+public class ExampleDate {
+    public static void main(String[] args) {
+
+        // Menampilkan tanggal sekarang
+        Date date = new Date();
+        System.out.println("Tanggal sekarang adalah " + date.toString());
+    }
+}
